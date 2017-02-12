37608acc5ca29df3535413e45c746402124a4bff
Andrew Roslik <andkirby@gmail.com>
2017-02-12 17:19:11 +0200

    - Refactored script to using module properties.

--- src/server.rb
+++ src/server.rb
@@ -18,32 +18,15 @@ end
 
 module GitlabHook
   module Server
+    @request
+    @response
+    @request_data
     def self.run_server
       server = WEBrick::HTTPServer.new(:Port => configatron.app.web.port)
       server.mount_proc '/inbound' do |request, response|
-        data = read_data(request)
-
-        begin
-          GitlabHook::Project::init(
-            URI(data['repository']['homepage']).path
-          )
-          GitlabHook::Inbound.new.forward data
-        rescue GitlabHook::Error => e
-          pp e.message
-          pp e.backtrace
-          pp '-----------'
-          exit 56
-          response.status = 404
-          return "error: #{e.message}"
-        rescue => e
-          pp e.message
-          pp e.backtrace
-          pp '+++++++____+++++++'
-          exit 56
-          # response.status = 503
-          return "error [#{e.class}]: #{e.message}\n#{e.backtrace}"
-        else
-        end
+        @request      = request
+        @response     = response
+        puts process_gitlab_webhook
       end
 
       trap 'INT' do
@@ -52,11 +35,30 @@ module GitlabHook
       server.start
     end
 
-    def self.read_data(request)
-      if request.body.empty?
+    def self.process_gitlab_webhook
+      begin
+        @response.set_error 'aaa'
+        @request_data = read_data
+
+        GitlabHook::Project::init(
+          URI(@request_data['repository']['homepage']).path
+        )
+        GitlabHook::Inbound.new.forward @request_data
+      rescue GitlabHook::Error => e
+        @response.status = 404
+        output = "error [#{e.class}]: #{e.message}\n#{e.backtrace}"
+      rescue => e
+        @response.status = 503
+        output = "error [#{e.class}]: #{e.message}\n#{e.backtrace}"
+      end
+      output
+    end
+
+    def self.read_data
+      if @request.body.nil? or @request.body.empty?
         raise 'Invalid request.'
       end
-      JSON.parse request.body
+      JSON.parse @request.body
     end
   end
 end
