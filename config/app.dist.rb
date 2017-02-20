636ccfc337aac2057dda8f3111d2c695be6e056f
Andrew Roslik <andkirby@gmail.com>
2017-02-21 01:34:52 +0200

    - Added host and IP to config.

--- config/app.dist.rb
+++ config/app.dist.rb
@@ -1,7 +1,9 @@
 require 'configatron'
 
-c = configatron.app
-c.gitlab.endpoint             = ENV['APP_GITLAB_ENDPOINT']
-c.gitlab.super.private_token  = ENV['APP_GITLAB_PRIVATE_TOKEN']
-c.slack.webhook_url           = ENV['APP_SLACK_WEBHOOK']
-c.web.port                    = ENV['APP_WEB_PORT'] ? ENV['APP_WEB_PORT'] : 8088
+c                            = configatron.app
+c.gitlab.endpoint            = ENV['APP_GITLAB_ENDPOINT']
+c.gitlab.super.private_token = ENV['APP_GITLAB_PRIVATE_TOKEN']
+c.slack.webhook_url          = ENV['APP_SLACK_WEBHOOK']
+c.web.port                   = ENV['APP_WEB_PORT'] ? ENV['APP_WEB_PORT'] : 8088
+c.web.ip                     = ENV['APP_WEB_IP'] ? ENV['APP_WEB_IP'] : '0.0.0.0'
+c.web.host                   = ENV['APP_WEB_HOST'] ? ENV['APP_WEB_HOST'] : 'gitlab-proxy.cc'
